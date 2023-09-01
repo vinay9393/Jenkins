6316cb6d65fc92b5ed032a511f0988ecadbc95d3
vinay <vinay@outlook.com>
2023-09-02 03:11:19 +0530

    dummy hc script

---
+++ dummy_script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Command 1: Print the current date and time
+date
+
+# Command 2: List files in the current directory
+ls
+
+# Command 3: Create a new directory
+mkdir my_directory
+
+# Command 4: Change to the new directory
+cd my_directory
+
+# Command 5: Create a new file
+touch my_file.txt
+
+# Command 6: Add some text to the file
+echo "Hello, World!" > my_file.txt
+
+# Command 7: Display the contents of the file
+cat my_file.txt
+
+# Command 8: Remove the file and the directory
+rm my_file.txt
+cd ..
+rmdir my_directory
+
